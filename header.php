76e6f66a99a4431fae36648afb17bf3218d6ac88
Keanan Koppenhaver <keanan@alphaparticle.com>
2023-01-13 07:08:55 -0600

    Clean up whitespace

--- header.php
+++ header.php
@@ -1,5 +1,3 @@
-
-
 <?php
 /**
  * The header.
@@ -7,10 +5,6 @@
  * This is the template that displays all of the <head> section and everything up until main.
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package WordPress
- * @subpackage Twenty_Twenty_One
- * @since Twenty Twenty-One 1.0
  */
 
 ?>
